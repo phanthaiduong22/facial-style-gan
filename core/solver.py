d926e116667bf82db33f08e076e2149ad6ea1de3
Duong Phan <duong.phanthai@shopee.com>
2022-06-17 01:18:03 +0700

    thread doable

--- core/solver.py
+++ core/solver.py
@@ -23,27 +23,32 @@ from core.data_loader import InputFetcher
 import core.utils as utils
 from metrics.eval import calculate_metrics
 
+import queue
+import threading
+
 threadList = ["Thread-1", "Thread-2", "Thread-3"]
 nameList = ["One", "Two", "Three", "Four", "Five"]
 
 class myThread (threading.Thread):
-    def __init__(self, threadID, name, q):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name = name
-		self.q = q
-    def run(self, solver):
-		print "Starting " + self.name
-        process_data(self.name, self.q)
-        print "Exiting " + self.name
+    def __init__(self, threadID, name, q, solver):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.q = q
+        self.solver = solver
+    def run(self):
+        print("Starting " + self.name)
+        print(self.name, self.q.get())
+        # process_data(self.name, self.q)
+        # solver.handle_training()
+        print("Exiting " + self.name)
 
-    def process_data(threadName, q):
-        while not exitFlag:
-            if not workQueue.empty():
-                data = q.get()
-                print "%s processing %s" % (threadName, data)
-            else:
-                time.sleep(1)
+    # def process_data(threadName, q):
+    #     if not workQueue.empty():
+    #         data = q.get()
+    #         print("%s processing %s" % (threadName, data))
+    #     else:
+    #         time.sleep(1)
 
 class Solver(nn.Module):
     def __init__(self, args):
@@ -51,13 +56,7 @@ class Solver(nn.Module):
         self.args = args
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.threads = []
-        self.workQueue = Queue.Queue(10)
-        # Handling threads
-        for tName in threadList:
-            thread = myThread(threadID, tName, workQueue)
-            thread.start()
-            self.threads.append(thread)
-            threadID += 1
+        self.workQueue = queue.Queue(10)
 
         self.nets, self.nets_ema = build_model(args)
         # below setattrs are to make networks be children of Solver, e.g., for self.to(self.device)
@@ -105,39 +104,47 @@ class Solver(nn.Module):
             optim.zero_grad()
 
     def train(self, loaders):
-        for word in nameList:
-            self.workQueue.put(word)
-        # args = self.args
+        # for word in nameList:
+        #     self.workQueue.put(word)
+        args = self.args
         # nets = self.nets
         # nets_ema = self.nets_ema
         # optims = self.optims
 
-        # # fetch random validation images for debugging
-        # fetcher = InputFetcher(loaders.src, loaders.ref, args.latent_dim, 'train')
-        # fetcher_val = InputFetcher(loaders.val, None, args.latent_dim, 'val')
-        # inputs_val = next(fetcher_val)
+        # fetch random validation images for debugging
+        self.fetcher = InputFetcher(loaders.src, loaders.ref, args.latent_dim, 'train')
+        self.fetcher_val = InputFetcher(loaders.val, None, args.latent_dim, 'val')
+        self.inputs_val = next(self.fetcher_val)
 
-        # # resume training if necessary
-        # if args.resume_iter > 0:
-        #     self._load_checkpoint(args.resume_iter)
+        # resume training if necessary
+        if args.resume_iter > 0:
+            self._load_checkpoint(args.resume_iter)
 
-        # # remember the initial value of ds weight
-        # initial_lambda_ds = args.lambda_ds
+        # remember the initial value of ds weight
+        self.initial_lambda_ds = args.lambda_ds
 
-        # print('Start training...')
-        # start_time = time.time()
-        # for i in range(args.resume_iter, args.total_iters):
-        #     handle_training(nets, nets_ema, optims, fetcher, inputs_val, initial_lambda_ds, start_time)
+        print('Start training...')
+        self.start_time = time.time()
+
+                # Handling threads
+        threadID = 1
+        for tName in threadList:
+            thread = myThread(threadID, tName, self.workQueue, self)
+            thread.start()
+            self.threads.append(thread)
+            threadID += 1
+
+        for i in range(args.resume_iter, args.total_iters):
+            self.workQueue.put(i)
     
-    
-    def handle_training(nets, nets_ema, optims, fetcher, inputs_val, initial_lambda_ds, start_time):
+    def handle_training(self, i):
         # fetch images and labels
         inputs = next(fetcher)
         x_real, y_org = inputs.x_src, inputs.y_src
         x_ref, x_ref2, y_trg = inputs.x_ref, inputs.x_ref2, inputs.y_ref
         z_trg, z_trg2 = inputs.z_trg, inputs.z_trg2
 
-        masks = nets.fan.get_heatmap(x_real) if args.w_hpf > 0 else None
+        masks = self.nets.fan.get_heatmap(x_real) if self.args.w_hpf > 0 else None
 
         # train the discriminator
         # d_loss, d_losses_latent = compute_d_loss(
@@ -147,10 +154,10 @@ class Solver(nn.Module):
         # optims.discriminator.step()
 
         d_loss, d_losses_ref = compute_d_loss(
-            nets, args, x_real, y_org, y_trg, x_ref=x_ref, masks=masks)
+            self.nets, self.args, x_real, y_org, y_trg, x_ref=x_ref, masks=masks)
         self._reset_grad()
         d_loss.backward()
-        optims.discriminator.step()
+        self.optims.discriminator.step()
 
         # train the generator
         # g_loss, g_losses_latent = compute_g_loss(
@@ -163,49 +170,49 @@ class Solver(nn.Module):
         
 
         g_loss, g_losses_ref = compute_g_loss(
-            nets, args, x_real, y_org, y_trg, x_refs=[x_ref, x_ref2], masks=masks)
+            self.nets, self.args, x_real, y_org, y_trg, x_refs=[x_ref, x_ref2], masks=masks)
         self._reset_grad()
         g_loss.backward()
-        optims.generator.step()
+        self.optims.generator.step()
 
 
         # compute moving average of network parameters
-        moving_average(nets.generator, nets_ema.generator, beta=0.999)
+        moving_average(self.nets.generator, self.nets_ema.generator, beta=0.999)
         # moving_average(nets.mapping_network, nets_ema.mapping_network, beta=0.999)
-        moving_average(nets.style_encoder, nets_ema.style_encoder, beta=0.999)
+        moving_average(self.nets.style_encoder, self.nets_ema.style_encoder, beta=0.999)
 
         # decay weight for diversity sensitive loss
-        if args.lambda_ds > 0:
-            args.lambda_ds -= (initial_lambda_ds / args.ds_iter)
+        if self.args.lambda_ds > 0:
+            self.args.lambda_ds -= (self.itial_lambda_ds / self.args.ds_iter)
 
         # print out log info
-        if (i+1) % args.print_every == 0:
-            elapsed = time.time() - start_time
+        if (i+1) % self.args.print_every == 0:
+            elapsed = time.time() - self.start_time
             elapsed = str(datetime.timedelta(seconds=elapsed))[:-7]
-            log = "Elapsed time [%s], Iteration [%i/%i], " % (elapsed, i+1, args.total_iters)
+            log = "Elapsed time [%s], Iteration [%i/%i], " % (elapsed, i+1, self.args.total_iters)
             all_losses = dict()
             # for loss, prefix in zip([d_losses_latent, d_losses_ref, g_losses_latent, g_losses_ref],
                                     # ['D/latent_', 'D/ref_', 'G/latent_', 'G/ref_']):
             for loss, prefix in zip([ d_losses_ref, g_losses_ref],[ 'D/ref_', 'G/ref_']):                     
                 for key, value in loss.items():
                     all_losses[prefix + key] = value
-            all_losses['G/lambda_ds'] = args.lambda_ds
+            all_losses['G/lambda_ds'] = self.args.lambda_ds
             log += ' '.join(['%s: [%.4f]' % (key, value) for key, value in all_losses.items()])
             print(log)
 
         # generate images for debugging
-        if (i+1) % args.sample_every == 0:
-            os.makedirs(args.sample_dir, exist_ok=True)
-            utils.debug_image(nets_ema, args, inputs=inputs_val, step=i+1)
+        if (i+1) % self.args.sample_every == 0:
+            os.makedirs(self.args.sample_dir, exist_ok=True)
+            utils.debug_image(self.nets_ema, self.args, inputs=self.inputs_val, step=i+1)
 
         # save model checkpoints
-        if (i+1) % args.save_every == 0:
+        if (i+1) % self.args.save_every == 0:
             self._save_checkpoint(step=i+1)
 
         # compute FID and LPIPS if necessary
-        if (i+1) % args.eval_every == 0:
+        if (i+1) % self.args.eval_every == 0:
             # calculate_metrics(nets_ema, args, i+1, mode='latent')
-            calculate_metrics(nets_ema, args, i+1, mode='reference')
+            calculate_metrics(self.nets_ema, self.args, i+1, mode='reference')
 
     @torch.no_grad()
     def sample(self, loaders):
