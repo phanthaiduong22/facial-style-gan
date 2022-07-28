9d93e19d983d82469d73edc5ea68d0fb3daa03fb
duong.phanthai <duong.phanthai@shopee.com>
2022-07-28 14:45:31 +0700

    update

--- core/utils.py
+++ core/utils.py
@@ -117,15 +117,14 @@ def translate_using_reference_object_detection(nets, args, x_src, x_ref, y_ref, 
 
     masks = nets.fan.get_heatmap(x_src) if args.w_hpf > 0 else None
     s_ref = nets.style_encoder(x_ref, y_ref)
+    print(s_ref)
     s_ref_list = s_ref.unsqueeze(1).repeat(1, N, 1)
     # x_concat = [x_src_with_wb]
     x_concat = []
     for i, s_ref in enumerate(s_ref_list):
-        # print()
-        print(i)
         x_fake = nets.generator(x_src, s_ref, masks=masks)
-        x_fake_with_ref = torch.cat([x_ref[i:i+1], x_fake], dim=0)
-        x_concat += [x_fake_with_ref]
+        # x_fake_with_ref = torch.cat(, x_fake], dim=0)
+        x_concat += [x_fake]
 
     x_concat = torch.cat(x_concat, dim=0)
     save_image(x_concat, N+1, filename)
