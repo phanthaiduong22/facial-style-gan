62f2155cf22f8ff40141d487116cb2c11fc2de3d
duong.phanthai <duong.phanthai@shopee.com>
2022-07-28 14:36:51 +0700

    update

--- core/utils.py
+++ core/utils.py
@@ -113,7 +113,7 @@ def translate_using_reference(nets, args, x_src, x_ref, y_ref, filename, print_b
 def translate_using_reference_object_detection(nets, args, x_src, x_ref, y_ref, filename):
     N, C, H, W = x_src.size()
     wb = torch.ones(1, C, H, W).to(x_src.device)
-    x_src_with_wb = torch.cat([wb, x_src], dim=0)
+    # x_src_with_wb = torch.cat([wb, x_src], dim=0)
 
     masks = nets.fan.get_heatmap(x_src) if args.w_hpf > 0 else None
     s_ref = nets.style_encoder(x_ref, y_ref)
@@ -122,6 +122,8 @@ def translate_using_reference_object_detection(nets, args, x_src, x_ref, y_ref, 
     x_concat = []
     for i, s_ref in enumerate(s_ref_list):
         # print()
+        if(i != len(enumerate(s_ref_list)) - 1):
+            continue
         x_fake = nets.generator(x_src, s_ref, masks=masks)
         x_fake_with_ref = torch.cat([x_ref[i:i+1], x_fake], dim=0)
         x_concat += [x_fake_with_ref]
