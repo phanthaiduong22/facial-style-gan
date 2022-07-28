13184841f56b5c69f298a6f4727ebf1cfdd86c87
duong.phanthai <duong.phanthai@shopee.com>
2022-07-28 14:40:42 +0700

    update

--- core/utils.py
+++ core/utils.py
@@ -124,7 +124,7 @@ def translate_using_reference_object_detection(nets, args, x_src, x_ref, y_ref, 
         # print()
         print(i)
         x_fake = nets.generator(x_src, s_ref, masks=masks)
-        # x_fake_with_ref = torch.cat([x_ref[i:i+1], x_fake], dim=0)
+        x_fake_with_ref = torch.cat([x_ref[i:i+1], x_fake], dim=0)
         x_concat += [x_fake]
 
     x_concat = torch.cat(x_concat, dim=0)
