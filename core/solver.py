7ec8d8efa6331bffe6f5ea4da5bdd2f96aaf3ece
duong.phanthai <duong.phanthai@shopee.com>
2022-07-28 12:07:15 +0700

    updatE

--- core/solver.py
+++ core/solver.py
@@ -235,7 +235,7 @@ class Solver(nn.Module):
         bgdModel = np.zeros((1,65),np.float64)
         fgdModel = np.zeros((1,65),np.float64)
         rect = (50,50,450,290)
-        cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
+        cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
         mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
         img = img*mask2[:,:,np.newaxis]
         plt.imshow(img),plt.colorbar(),plt.show()
