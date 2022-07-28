7aa83bfe8b5604bca26c9e7c4904ef2eb9ac2029
duong.phanthai <duong.phanthai@shopee.com>
2022-07-28 22:54:28 +0700

    update

--- core/utils.py
+++ core/utils.py
@@ -111,10 +111,10 @@ def translate_using_reference(nets, args, x_src, x_ref, y_ref, filename, print_b
         # x_concat = [x_src_with_wb]
         for i, s_ref in enumerate(s_ref_list):
             x_fake = nets.generator(x_src, s_ref, masks=masks)
-            x_fake_with_ref = torch.cat([x_ref[i:i+1], x_fake], dim=0)
+            # x_fake_with_ref = torch.cat([x_ref[i:i+1], x_fake], dim=0)
             # x_concat += [x_fake_with_ref]
 
-        x_concat = torch.cat(x_concat, dim=0)
+        # x_concat = torch.cat(x_concat, dim=0)
         # save_image(x_concat, N+1, filename)
         # del x_concat
     else:
